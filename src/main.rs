9770e4422f49380ef1fa8d865164d7a1c0457d0f
Anthony Riley II <riley@anth1y.io>
2020-08-28 20:44:28 -0700

    using rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::io::stdout;
-use std::io::{self, Write};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() {
+    let mut rl = Editor::<()>::new();
     loop {
-        println!("Oxideon> ");
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_n) => {
-                println!("You typed: {}", input);
+        let readline = rl.readline("Oxideon> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
             }
-            Err(error) => println!("error: {}", error),
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
         }
-        stdout().flush().unwrap();
     }
+    rl.save_history("history.txt").unwrap();
 }
